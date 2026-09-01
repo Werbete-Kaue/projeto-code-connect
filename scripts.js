bd08aba11363d1ec3eecbb7dd2e40570577df89b
Werbete Kaue <kauebrxff@gmail.com>
2026-09-01 18:06:36 -0300

    feat: Adiciona a funcionalidade de publicar projeto com tratamento de erros

--- scripts.js
+++ scripts.js
@@ -84,18 +84,6 @@ inputTags.addEventListener("keypress", async (evento) => {
 
 const botaoPublicar = document.querySelector(".botao-publicar");
 
-botaoPublicar.addEventListener("click", async (evento) => {
-    evento.preventDefault();
-
-    const nomeDoProjeto = document.getElementById("nome").value;
-    const descricaoDoProjeto = document.getElementById("descricao").value
-    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
-
-    console.log(nomeDoProjeto)
-    console.log(descricaoDoProjeto)
-    console.log(tagsProjeto)
-})
-
 async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -109,3 +97,21 @@ async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto){
         }, 2000)
     })
 }
+
+botaoPublicar.addEventListener("click", async (evento) => {
+    evento.preventDefault();
+
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value
+    const tagsProjeto = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+
+    try {
+        const resultado = await publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjeto);
+        console.log(resultado)
+        alert("Deu tudo certo!")
+    } catch (error) {
+        console.log("Deu errado:", error)
+        alert("Deu tudo errado!");
+    }
+
+})
